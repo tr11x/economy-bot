9785ee464f990fbe5084fe8c6828c0602eece4ba
Hie_هاي <58346370+hieru3@users.noreply.github.com>
2021-08-27 14:23:54 +0300

    Привет ⁞ 💘ֆ ⁽ʜɪᴇ ⁞ ⁾↵⇣℡#2000

--- commands/credits.js
+++ commands/credits.js
@@ -2,7 +2,6 @@ const db = require('quick.db')
 const Discord = require('discord.js')
 module.exports = {
     name:"credits",
- 
     run: async(client, message, args) => {
         let user = message.mentions.users.first() || message.author;
       
